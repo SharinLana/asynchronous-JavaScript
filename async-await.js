ed95af48f41c7aa417fe30d39326cf7c3ad1cc69
Lana Sharin <lana.sharin.webdev@gmail.com>
2022-11-22 12:32:37 -0800

    solve problem #2 using async-await + filter()

--- async-await.js
+++ async-await.js
@@ -1,19 +1,41 @@
 // Taks 1
-// Get an array of the film titles using 
+// Get an array of the film titles using
 // 'https://swapi.dev/api/films/'
 
-const swapiFilms = async () => { // made this func async because we don't know when we receive the response
-    let url = 'https://swapi.dev/api/films/';
+const swapiFilms = async () => {
+  // made this func async because we don't know when we receive the response
+  let url = "https://swapi.dev/api/films/";
 
-    // Here we need to wait for the result of fetching
-    // before executing the code below.
-    // If we skip waiting (by not using the 'await' keyword)
-    // then the result of logging "films" to the console 
-    // will be undefined as fetch() can be late with response
-    const filmObj = await fetch(url).then(data => data.json());
-    const films = filmObj.results.map(obj => obj.title);
+  // Here we need to wait for the result of fetching
+  // before executing the code below.
+  // If we skip waiting (by not using the 'await' keyword)
+  // then the result of logging "films" to the console
+  // will be undefined as fetch() can be late with response
+  const filmObj = await fetch(url).then((data) => data.json());
+  const films = filmObj.results.map((obj) => obj.title);
 
-    console.log(films);
+  console.log(films);
 };
 
-swapiFilms()
+swapiFilms();
+
+// Task 2
+// Create a function that will retrieve the posts from
+// "https://jsonplaceholder.typicode.com/posts"
+
+const getPosts = async (userId) => {
+  try {
+    const allPosts = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      ).then((data) => data.json());
+    
+      //   using filter() method to find only the objects with a particular userId
+      const usersPosts = allPosts.filter((obj) => obj.userId === userId);
+    
+      return usersPosts;
+  } catch (err) {
+    console.error('ERROR!!! ', err);
+  }
+};
+
+getPosts(1).then(res => console.log(res));
